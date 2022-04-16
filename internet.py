dc93201b78fe171ac0b8c84c323c5d6fe8390d91
Omg-A <67595078+Omg-A@users.noreply.github.com>
2022-04-16 16:55:06 +0300

    Add files via upload

---
+++ internet.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import time
+import speedtest
+
+list_download_speed = []
+list_upload_speed = []
+
+for i in range(5):
+    st = speedtest.Speedtest()
+    
+    download_speed = round(st.download() / 1000000, 2)
+    list_download_speed.append(download_speed)
+    
+    upload_speed = round(st.upload() / 1000000, 2)
+    list_upload_speed.append(upload_speed)
+    
+    print(list_download_speed)
+    print(list_upload_speed)
+
+x = [1, 2, 3, 4, 5]
+
+plt.plot(x, list_download_speed, label="Download Speed")
+plt.plot(x, list_upload_speed, label="Upload Speed")
+plt.title("Internet speed")
+plt.legend()
+plt.show()
